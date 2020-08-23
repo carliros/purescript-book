f56674d60f7211b64cdfb99d12144cce96f57eb6
Carlos Gómez <carliros.g@gmail.com>
2020-08-23 09:11:11 -0400

    my solutions to chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,18 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Global (readFloat)
+import Math (e, pi, sqrt)
+
+double :: Number -> Number
+double n = n * n
+
+diagonal :: Number -> Number -> Number
+diagonal a b = sqrt ( double a + double b)
+
+circleArea :: Number -> Number
+circleArea = (*) pi <<< double
+
+addE :: String -> Number
+addE = (+) e <<< readFloat
