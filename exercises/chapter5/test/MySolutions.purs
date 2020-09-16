9890bafe3cce292f181866807a693258d25d4493
Carlos Gómez <carliros.g@gmail.com>
2020-09-16 18:32:48 -0400

    add fix for sameCity and fromSingleton

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -1,6 +1,7 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Person(Person)
 
 factorial :: Int -> Int
 factorial 0 = 1
@@ -24,3 +25,11 @@ pascal n k
     | n < k  = 0
     | n == k = 1
     | otherwise = factorial n / (factorial k * factorial (n - k))
+
+sameCity :: Person -> Person -> Boolean
+sameCity { address: { city: c1 } } { address: { city: c2 } } = c1 == c2
+
+fromSingleton :: String -> Array String -> String
+fromSingleton def [] = def
+fromSingleton def [s] = s
+fromSingleton def _ = def
